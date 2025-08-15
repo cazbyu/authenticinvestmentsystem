96d80de1a12fb11e0586d4ab1c857880c984acc0
cazbyu <131417207+cazbyu@users.noreply.github.com>
2025-08-15 14:15:24 -0600

    Updated calendarLocale.ts

--- lib/calendarLocale.ts
+++ lib/calendarLocale.ts
@@ -2,6 +2,8 @@ import { LocaleConfig } from 'react-native-calendars';
 
 LocaleConfig.locales['en-short'] = {
   ...LocaleConfig.locales['en'],
+  // Add this line
+  dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
   dayNamesShort: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
 };
 
