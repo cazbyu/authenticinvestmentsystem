340b179927876e5ad1b413f7d6d7591c81a3d12e
cazbyu <131417207+cazbyu@users.noreply.github.com>
2025-08-14 21:20:29 -0600

    Updated TaskEventForm.tsx

--- components/tasks/TaskEventForm.tsx
+++ components/tasks/TaskEventForm.tsx
@@ -301,7 +301,7 @@ const TaskEventForm: React.FC<TaskEventFormProps> = ({ mode, initialData, onSubm
                           <View style={[
                             styles.calendarPopup,
                             {
-                              top: datePickerPosition.height + 5,
+                              top: datePickerPosition.height + 15,
                               left: datePickerPosition.width + 5,
                             }
                           ]}>
