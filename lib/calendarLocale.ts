99d9a77111a53235466e6e1cec1ae237f4d7af38
cazbyu <131417207+cazbyu@users.noreply.github.com>
2025-08-15 14:51:26 -0600

    Updated calendarLocale.ts

--- lib/calendarLocale.ts
+++ lib/calendarLocale.ts
@@ -1,8 +1,18 @@
 import { LocaleConfig } from 'react-native-calendars';
 
 LocaleConfig.locales['en-short'] = {
-  ...LocaleConfig.locales['en'],
-  dayNamesShort: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+  monthNames: [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ],
+  monthNamesShort: [
+    'Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'Jun.',
+    'Jul.', 'Aug.', 'Sep.', 'Oct.', 'Nov.', 'Dec.'
+  ],
+  dayNames: [
+    'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
+  ],
+  dayNamesShort: ['S', 'M', 'T', 'W', 'T', 'F', 'S']
 };
 
 LocaleConfig.defaultLocale = 'en-short';
