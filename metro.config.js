a6940d8730e51e66857ad2f482b5085899d4cc04
cazbyu <131417207+cazbyu@users.noreply.github.com>
2025-08-20 22:42:13 -0600

    Fix Metro configuration compatibility

--- metro.config.js
+++ metro.config.js
@@ -2,13 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Add support for additional file extensions
-config.resolver.assetExts.push('db', 'mp3', 'ttf', 'obj', 'png', 'jpg');
-
-// Add support for TypeScript path mapping
-config.resolver.alias = {
-  ...config.resolver.alias,
-  '@': __dirname,
-};
-
 module.exports = config;
