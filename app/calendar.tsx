5f940ae6792502b47640ce752bd0a65b492ab014
cazbyu <131417207+cazbyu@users.noreply.github.com>
2025-08-27 14:05:37 -0700

    Updated calendar.tsx

--- app/calendar.tsx
+++ app/calendar.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Alert, FlatList, Modal, useRef } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Alert, FlatList, Modal } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Calendar } from 'react-native-calendars';
 import { Header } from '@/components/Header';
